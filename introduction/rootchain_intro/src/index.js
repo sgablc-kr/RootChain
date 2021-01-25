df2e839202cbf6c8a994f0fdc50d6279b6bb0007
seoha <kimseoha@sgacorp.kr>
2021-01-25 11:27:53 +0900

    docs: fix GA

--- introduction/rootchain_intro/src/index.js
+++ introduction/rootchain_intro/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 // Google Analytics
 import ReactGA from "react-ga";
 ReactGA.initialize('UA-187898394-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
